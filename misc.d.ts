15e9be9b24d05690784fa9e8ac8f912b27f48065
Matthew Brown <barakis@barakisbrown.com>
2016-01-15 23:03:26 -0600

    misc module definition file
    Module [misc] typescript definition file.

---
+++ misc.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="typing/node/node.d.ts" />
+declare module NodeBattle {
+    function singleDiceToss(dice: number): number;
+    function manyDiceToss(times: number, dice: number): number;
+}
