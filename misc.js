ab9c16526e4a44b99a4037b62d5a8afcd1ecca4f
Matthew Brown <barakis@barakisbrown.com>
2016-01-16 19:35:50 -0600

    Added ask function
    Ask function -- A way for me to ask a question and then validate it.

--- misc.js
+++ misc.js
@@ -14,5 +14,22 @@ var NodeBattle;
             return total;
         }
         misc.manyDiceToss = manyDiceToss;
+        function ask(question, format, callback) {
+            var stdin = process.stdin;
+            var stdout = process.stdout;
+            stdin.resume();
+            stdout.write(question, " > ");
+            stdin.once('data', function (data) {
+                data = data.toString().trim();
+                if (format.test(data)) {
+                    callback(data);
+                }
+                else {
+                    stdout.write('\n oops try again');
+                    ask(question, format, data);
+                }
+            });
+        }
+        misc.ask = ask;
     })(misc = NodeBattle.misc || (NodeBattle.misc = {}));
 })(NodeBattle || (NodeBattle = {}));
--- misc.ts
+++ misc.ts
@@ -24,4 +24,26 @@ module NodeBattle.misc {
 
      return total;
   }
+
+  export function ask(question, format, callback) {
+
+    var stdin = process.stdin;
+    var stdout = process.stdout;
+
+    stdin.resume();
+    stdout.write(question, " > ");
+
+    stdin.once('data', function(data) {
+
+        data = data.toString().trim();
+        if (format.test(data)) {
+          callback(data);
+        } else {
+          stdout.write('\n oops try again');
+          ask(question, format, data);
+        }
+
+    });
+
+  }
 }
