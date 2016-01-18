0b7815ece7c3845f6ae57c060c5332522229d9f5
Matthew Brown <barakis@barakisbrown.com>
2016-01-17 21:36:36 -0600

    Added getDice function and documentation for my functions

--- misc.ts
+++ misc.ts
@@ -1,6 +1,8 @@
 /// <reference path="typing\node\node.d.ts" />
+/// <reference path="base.ts" />
 
 module NodeBattle.misc {
+
   /**
     function singleDiceToss(number)
     returns a number beteen 1 and dice once.
@@ -25,6 +27,11 @@ module NodeBattle.misc {
      return total;
   }
 
+  /**
+    function ask(question, format, callback)
+    Asks a question to the console and awaits input.
+    It then validates the input.
+  */
   export function ask(question, format, callback) {
 
     var stdin = process.stdin;
@@ -44,6 +51,43 @@ module NodeBattle.misc {
         }
 
     });
+  }
 
+  /**
+    function getDice(start: Dice, added:number) : Dice
+    Helper function that allows me to get the dice value back since Dice is
+    an enum that I use. It takes the starting dice and then adds a value to
+    get the new Dice value that it needs to be done.
+  */
+  export function getDice(start: Dice, added:number) : Dice {
+      if (added === 0)
+          return start;
+      else {
+
+          switch(start) {
+
+            case Dice.D4: {
+                return getDice(Dice.D6, added - 1);
+            }
+            case Dice.D6: {
+                return getDice(Dice.D8, added - 1);
+            }
+            case Dice.D8: {
+                return getDice(Dice.D10, added - 1);
+            }
+            case Dice.D10: {
+                return getDice(Dice.D12, added - 1);
+            }
+            case Dice.D12: {
+                return getDice(Dice.D20, added - 1);
+            }
+            case Dice.D20: {
+                return getDice(Dice.D100, added - 1);
+            }
+            case Dice.D100: {
+                return getDice(Dice.D100, 0);
+            }
+        }
+    }
   }
 }
